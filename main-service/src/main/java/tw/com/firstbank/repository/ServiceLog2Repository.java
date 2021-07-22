ea91b61b666e76406c3bf8d325bee8d995431640
andy_lee <andy_lee@fstop.com.tw>
2021-07-22 18:55:53 +0800

    add api

--- main-service/src/main/java/tw/com/firstbank/repository/ServiceLog2Repository.java
+++ main-service/src/main/java/tw/com/firstbank/repository/ServiceLog2Repository.java
@@ -2,7 +2,6 @@ package tw.com.firstbank.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 import tw.com.firstbank.entity.ServiceLog2;
 import tw.com.firstbank.entity.ServiceLogKey;
