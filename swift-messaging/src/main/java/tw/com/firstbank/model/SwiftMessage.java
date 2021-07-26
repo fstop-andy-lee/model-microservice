9b082dbd239670809940b390c46d0211eaad50f2
andy_lee <andy_lee@fstop.com.tw>
2021-07-26 17:39:01 +0800

    add api

--- swift-messaging/src/main/java/tw/com/firstbank/model/SwiftMessage.java
+++ swift-messaging/src/main/java/tw/com/firstbank/model/SwiftMessage.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.annotations.ApiModel;
@@ -20,6 +21,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @ApiModel(description = "Swift Message")
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Validated
 @Getter
 @Setter
@@ -41,5 +43,31 @@ public class SwiftMessage {
   @JsonProperty("valueDate")
   @JsonFormat(pattern="yyyy/MM/dd")
   private Date valueDate;
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    SwiftMessage other = (SwiftMessage) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+  
   
 }
