c777a9f999a3386c81495b5e8e18efb458e909de
andy_lee <andy_lee@fstop.com.tw>
2021-08-28 22:05:42 +0800

    c

--- inward-rmt-service/src/main/java/tw/com/firstbank/service/RepositoryHelperImpl.java
+++ inward-rmt-service/src/main/java/tw/com/firstbank/service/RepositoryHelperImpl.java
@@ -156,7 +156,7 @@ public class RepositoryHelperImpl implements RepositoryHelper {
       throw new IllegalStateException("Trigger tx rollback");      
     }
     
-    for(int i=0; i<500000; i++) {
+    for(int i=0; i<5000000; i++) {
       Math.sqrt(12345678);
     }
   }
